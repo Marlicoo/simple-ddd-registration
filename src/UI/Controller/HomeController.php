e0d8e5ad927a732a1869c7f7f3aab035945a6f5b
Marlena <marlena.kuc@onwelo.com>
2018-05-27 13:46:59 +0200

    Moved Controllers to UI localtion

---
+++ src/UI/Controller/HomeController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\UI\Controller;
+
+use App\Application\User\RegisterCommand;
+use League\Tactician\CommandBus;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+
+class HomeController extends Controller
+{
+
+}
